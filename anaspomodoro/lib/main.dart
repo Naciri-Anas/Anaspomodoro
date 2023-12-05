4d9d49163fe932d37a31d37a9a2c75d40a773803
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-12-05 23:07:53 +0100

    Update main.dart

--- anaspomodoro/lib/main.dart
+++ anaspomodoro/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:anaspomodoro/pomodoroscreen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PomodoroScreen();
+      home: PomodoroScreen()
     );
   }
 }

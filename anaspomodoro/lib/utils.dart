f8b4d28495c38f74ef885b8621f9219542d522b8
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2023-12-14 23:20:10 +0100

    Update utils.dart

--- anaspomodoro/lib/utils.dart
+++ anaspomodoro/lib/utils.dart
@@ -4,3 +4,17 @@ import 'package:google_fonts/google_fonts.dart';
 TextStyle textStyle(double size, [Color? color, FontWeight? fw]) {
   return GoogleFonts.montserrat(fontSize: size, color: color, fontWeight: fw);
 }
+
+List selectableTimes = [
+  "300",
+  "600",
+  "900",
+  "1200",
+  "1500",
+  "1800",
+  "2100",
+  "2400",
+  "2700",
+  "3000",
+  "3300"
+];
